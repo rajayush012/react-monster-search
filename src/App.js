6cf48d52a0f1d281c0cc4b321b81734eb98b91de
Ayush Raj <raj.ayush012@gmail.com>
2020-08-30 02:39:58 +0530

    Search bar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField : ''
     }
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
@@ -17,10 +20,20 @@ class App extends Component {
     .then(users => this.setState({monsters : users}))
   }
 
+  handleChange=(e)=>{
+    this.setState( {searchField: e.target.value} )
+  }
+
   render(){
+    const {monsters , searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox 
+          placeholder='Search monster'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
         
       </div>
     );

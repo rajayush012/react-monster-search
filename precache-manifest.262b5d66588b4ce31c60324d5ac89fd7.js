741a412b49915eb6c440becad460e79c1e36ddfc
Ayush Raj <raj.ayush012@gmail.com>
2020-08-30 03:27:15 +0530

    Updates

---
+++ precache-manifest.262b5d66588b4ce31c60324d5ac89fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6d7c97f8f8bacf1ac994090bb8e6a7",
+    "url": "/react-monster-search/index.html"
+  },
+  {
+    "revision": "d7f0e13ece8a84c8035c",
+    "url": "/react-monster-search/static/css/main.29e7a44a.chunk.css"
+  },
+  {
+    "revision": "4dbfe0ed1b7ce60601cd",
+    "url": "/react-monster-search/static/js/2.8521e2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-search/static/js/2.8521e2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f0e13ece8a84c8035c",
+    "url": "/react-monster-search/static/js/main.eda012a6.chunk.js"
+  },
+  {
+    "revision": "894c9e0e8202dfe27db1",
+    "url": "/react-monster-search/static/js/runtime-main.f53cadbe.js"
+  }
+]);

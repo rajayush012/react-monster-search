dd10d4c62eed381adbc3d80545e320d52837a301
Ayush Raj <raj.ayush012@gmail.com>
2020-08-30 01:29:39 +0530

    Cards ready

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { thisTypeAnnotation } from '@babel/types';
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-      monsters : [
-        {
-          name : 'Zombie',
-          id : 1
-        },
-        {
-          name : 'Frankestein',
-          id : 2
-        },
-        {
-          name : 'Dracula',
-          id : 3
-        }
-      ]
+      monsters : []
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+  }
+
   render(){
     return (
       <div className="App">
-        {this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name } </h1>)}
+        <CardList monsters={this.state.monsters}></CardList>
+        
       </div>
     );
   }
